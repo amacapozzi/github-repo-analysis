2bb70cdf61e098b6eea522ca8cced5b702719bce
amacapozzi <amacapozzi07@gmail.com>
2024-06-15 10:38:13 -0300

    Changed the way files are parsed through the buffer and added a generic detection.

--- src/services/analysis.service.ts
+++ src/services/analysis.service.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import FormData from "form-data";
+import yauzl from "yauzl";
 import { appConfig } from "../config/app.config";
-import fs from "fs";
-import temp from "temp";
 import { FileResult } from "../types/File";
 
 const BASE_URL = "https://www.virustotal.com/api";
@@ -10,38 +9,42 @@ const BASE_URL = "https://www.virustotal.com/api";
 export class AnalysisService {
   public static async checkFile(buffer: Buffer): Promise<FileResult | Error> {
     try {
-      const tempFilePath = temp.path({ suffix: ".tmp" });
-      fs.writeFileSync(tempFilePath, buffer);
-
-      const readStream = fs.createReadStream(tempFilePath);
-
       const formData = new FormData();
-      formData.append("file", readStream);
+      formData.append("file", buffer, {
+        filename: "file.exe",
+        contentType: "application/octet-stream",
+      });
 
       const response = await axios.post(`${BASE_URL}/v3/files`, formData, {
         headers: {
           "x-apikey": appConfig.VIRUS_TOTAL_APIKEY,
           ...formData.getHeaders(),
         },
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
       });
 
-      const info = await this.getFileInfo(response.data.data.links.self);
-
-      fs.unlinkSync(tempFilePath);
+      const info = await this.getFileInfo(response.data.data.self);
 
       return info.data.attributes.stats;
-    } catch (err) {
-      console.log(err);
-      return new Error("Error to scan file");
+    } catch (err: any) {
+      return new Error(`Error scanning the file ${err} `);
     }
   }
 
-  private static async getFileInfo(self: string) {
-    const response = await axios.get(self, {
+  private static async getFileInfo(fileId: string) {
+    const response = await axios.get(`${BASE_URL}/v3/files/${fileId}`, {
       headers: {
         "x-apikey": appConfig.VIRUS_TOTAL_APIKEY,
       },
     });
     return response.data;
+  }
+
+  public static async isZipProtected(buffer: Buffer): Promise<Boolean | void> {
+    yauzl.fromBuffer(buffer, (err) => {
+      if (err) return true;
+      return false;
+    });
   }
 }
